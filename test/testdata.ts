7fc6970d92a71dd5603f3eedae201034fb67694f
Jonathan Ling <jonathan.ling@hoteleffectiveness.com>
2019-06-10 13:56:16 -0400

    Add failing unit tests.

--- test/testdata.ts
+++ test/testdata.ts
@@ -1,10 +1,42 @@
 export const testData = {
   ACPI_POWER_BUTTON: {
-    stdOut:`0%...10%...20%...30%...40%...50%...60%...70%...80%...90%...100%`,
-    stdErr: null
+    INPUT: `vboxmanage controlvm testvm acpipowerbutton`,
+    OUTPUT: {
+      error: '',
+      stdOut: ``,
+      stdErr: null
+    }
   },
-  ACPI_SLEEP_BUTTON_OUTPUT: {
+  GET_VERSION: {
+    INPUT: `vboxmanage --version`,
+    OUTPUT: {
+      error: '',
+      stdOut: ``,
+      stdErr: null
+    }
+  },
+  GET_OS_TYPE: {
+    INPUT: `vboxmanage showvminfo -machinereadable testvm`,
+    OUTPUT: {
+      error: '',
+      stdOut: ``,
+      stdErr: null
+    }
+  },
+  ACPI_SLEEP_BUTTON: {
+    INPUT: `vboxmanage controlvm testvm acpisleepbutton`,
+    OUTPUT: {
+      error: '',
+      stdOut: ``,
+      stdErr: null
+    }
+  },
+  EXEC_OUTPUT: {
     stdOut: ``,
     stdErr: null
-  }
+  },
+  POWER_OFF_OUTPUT: {
+    stdOut: `0%...10%...20%...30%...40%...50%...60%...70%...80%...90%...100%`,
+    stdErr: null
+  },
 };
--- test/virtualbox.spec.ts
+++ test/virtualbox.spec.ts
@@ -1,22 +1,50 @@
 import { Virtualbox } from '../lib/virtualbox';
 import { testData } from './testdata';
 import { stub } from 'sinon';
+import  { assert } from 'chai';
 
 const VM_NAME = "testvm";
 
 describe("Virtualbox", () => {
+  const getBasicStub = () => {
+    return stub()
+      .withArgs(testData.GET_VERSION.INPUT)
+      .resolves(testData.GET_VERSION.OUTPUT)
+      .withArgs(testData.GET_OS_TYPE.INPUT)
+      .resolves(testData.GET_OS_TYPE.OUTPUT);
+  }
+  
+  it("should construct an instance", function () {
+    const virtualbox = new Virtualbox();
+
+    assert.isNotNull(virtualbox.executor);
+  });
+
   describe("#acpipowerbutton", () => {
-    it("should turn off a VM", (done) => {
-      const virtualbox = new Virtualbox();
-      virtualbox.executor = stub().resolves(testData.ACPI_POWER_BUTTON.stdOut);     
-      virtualbox.acpipowerbutton(VM_NAME).then(() => { done(); });
+    const fakeACPIPowerButtonExecutor = getBasicStub()
+      .withArgs(testData.ACPI_POWER_BUTTON.INPUT)
+      .resolves(testData.ACPI_POWER_BUTTON.OUTPUT);
+    it("should turn off a VM", () => {
+      const virtualbox = new Virtualbox(fakeACPIPowerButtonExecutor);
+      const result = virtualbox.acpipowerbutton(VM_NAME).then(value => {
+        virtualbox.logging.debug(result);
+      })
     });
   });
   describe("#acpisleepbutton", () => {
+    const fakeACPISleepButtonExecutor = getBasicStub()
+      .withArgs(testData.ACPI_SLEEP_BUTTON.INPUT)
+      .resolves(testData.ACPI_SLEEP_BUTTON.OUTPUT);
     it("should sleep a VM", (done) => {
+      const virtualbox = new Virtualbox(fakeACPISleepButtonExecutor);
+      virtualbox.acpisleepbutton(VM_NAME).then(() => { done(); }).catch(err => console.log("Something!"));
+    })
+  })
+  describe("#vmExec", () => {
+    it("should ping google", (done) => {
       const virtualbox = new Virtualbox();
-      virtualbox.executor = stub().resolves(testData.ACPI_SLEEP_BUTTON_OUTPUT.stdOut);     
-      virtualbox.acpipowerbutton(VM_NAME).then(() => { done(); });
+      virtualbox.executor = stub().resolves(testData.EXEC_OUTPUT.stdOut);
+      virtualbox.vmExec({ 'vm': VM_NAME, 'user': 'username', 'passwd': 'password', 'path': '/vagrant', 'params': 'https://google.com' });
     })
   })
 });
