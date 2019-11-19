b8a77da04d200835180787e4520179fcde488875
Jonathan Ling <jonathan.ling@hoteleffectiveness.com>
2019-11-19 09:17:07 -0500

    check in fixes.

--- test/integration/guestproperty.ts
+++ test/integration/guestproperty.ts
@@ -1,16 +1,3 @@
-// "use strict";
-
-// //TODO: Move to mocha for automation
-// var nvbox = require('../dist/virtualbox'),
-//     args = process.argv.slice(2);
-
-// // TODO: Refactor with a promise
-// virtualbox.guestproperty.get(args[0], args[1], function(error){
-//   if(error) {
-//     throw error;
-//   }
-// });
-
 // tslint:disable: only-arrow-functions
 "use strict";
 import assert from "assert";
