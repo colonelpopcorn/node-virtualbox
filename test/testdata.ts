3c4c0d2ede62c348b6c6f1f27b9c97dd228d85c2
Jonathan Ling <jonathan.ling@hoteleffectiveness.com>
2019-12-13 10:24:21 -0500

    Fix the build.

--- test/testdata.ts
+++ test/testdata.ts
@@ -1,42 +1,42 @@
 export const testData = {
   ACPI_POWER_BUTTON: {
-    INPUT: `vboxmanage controlvm testvm acpipowerbutton`,
+    INPUT: "vboxmanage controlvm testvm acpipowerbutton",
     OUTPUT: {
       error: "",
       stdErr: null,
-      stdOut: ``,
-    },
+      stdOut: ""
+    }
   },
   ACPI_SLEEP_BUTTON: {
-    INPUT: `vboxmanage controlvm testvm acpisleepbutton`,
+    INPUT: "vboxmanage controlvm testvm acpisleepbutton",
     OUTPUT: {
       error: "",
       stdErr: null,
-      stdOut: ``,
-    },
+      stdOut: ""
+    }
   },
   EXEC_OUTPUT: {
     stdErr: null,
-    stdOut: ``,
+    stdOut: ""
   },
   GET_OS_TYPE: {
-    INPUT: `vboxmanage showvminfo -machinereadable testvm`,
+    INPUT: "vboxmanage showvminfo -machinereadable testvm",
     OUTPUT: {
       error: "",
       stdErr: null,
-      stdOut: ``,
-    },
+      stdOut: ""
+    }
   },
   GET_VERSION: {
-    INPUT: `vboxmanage --version`,
+    INPUT: "vboxmanage --version",
     OUTPUT: {
       error: "",
       stdErr: null,
-      stdOut: ``,
-    },
+      stdOut: ""
+    }
   },
   POWER_OFF_OUTPUT: {
     stdErr: null,
-    stdOut: `0%...10%...20%...30%...40%...50%...60%...70%...80%...90%...100%`,
-  },
+    stdOut: "0%...10%...20%...30%...40%...50%...60%...70%...80%...90%...100%"
+  }
 };
--- test/unit/virtualbox.spec.ts
+++ test/unit/virtualbox.spec.ts
@@ -1,18 +1,19 @@
+import { Virtualbox } from "../../lib/virtualbox";
 import { assert } from "chai";
+import { getLogger } from "log4js";
 import { stub } from "sinon";
-import Virtualbox from "../../lib/virtualbox";
 import { testData } from "../testdata";
 
 const VM_NAME = "testvm";
+const logger = getLogger("Virtualbox Unit Tests");
 
 describe("Virtualbox", () => {
-  const getBasicStub = () => {
-    return stub()
+  const getBasicStub = () =>
+    stub()
       .withArgs(testData.GET_VERSION.INPUT)
       .resolves(testData.GET_VERSION.OUTPUT)
       .withArgs(testData.GET_OS_TYPE.INPUT)
       .resolves(testData.GET_OS_TYPE.OUTPUT);
-  };
   it("should construct an instance", () => {
     const virtualbox = new Virtualbox();
 
@@ -25,8 +26,8 @@ describe("Virtualbox", () => {
       .resolves(testData.ACPI_POWER_BUTTON.OUTPUT);
     it("should turn off a VM", () => {
       const virtualbox = new Virtualbox(fakeACPIPowerButtonExecutor);
-      const result = virtualbox.acpipowerbutton(VM_NAME).then((value) => {
-        virtualbox.Logging.debug(result);
+      const result = virtualbox.acpipowerbutton(VM_NAME).then(value => {
+        logger.debug(result);
       });
     });
   });
@@ -34,12 +35,16 @@ describe("Virtualbox", () => {
     const fakeACPISleepButtonExecutor = getBasicStub()
       .withArgs(testData.ACPI_SLEEP_BUTTON.INPUT)
       .resolves(testData.ACPI_SLEEP_BUTTON.OUTPUT);
-    it("should sleep a VM", (done) => {
+    it("should sleep a VM", done => {
       const virtualbox = new Virtualbox(fakeACPISleepButtonExecutor);
-      virtualbox.acpisleepbutton(VM_NAME).then(() => { done(); }).catch((err) => {
-        // tslint:disable-next-line
-        console.log("Something!");
-      });
+      virtualbox
+        .acpisleepbutton(VM_NAME)
+        .then(() => {
+          done();
+        })
+        .catch(err => {
+          logger.error("Something went wrong!", err);
+        });
     });
   });
   describe.skip("#vmExec", () => {
@@ -51,7 +56,7 @@ describe("Virtualbox", () => {
         passwd: "password",
         path: "/vagrant",
         user: "username",
-        vm: VM_NAME,
+        vm: VM_NAME
       });
     });
   });
